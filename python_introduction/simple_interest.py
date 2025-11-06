d903d7dbc9051a88228ffab702d94bee5c79b486
mamado143 <midodahir123@gmail.com>
2025-11-06 14:22:42 +0300

    Fix 1 the solution 2 for task 2 in file simple_interest.py

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,6 +1,6 @@
-#!/bin/usr/python3
+#!/bin/usr/python4
 principal = 1000
 rate = 0.05
 time = 3
-p = principal * rate * time
-print(f"The simple interest is: {p}")
+interest = principal * rate * time
+print(f"The simple interest is: {interest}")
