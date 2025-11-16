c37dad04168516af711d3c368e5a9a65a91d2458
mamado143 <midodahir123@gmail.com>
2025-11-16 17:49:10 +0300

    Solve and add task 3: file pattern_drawing.py

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+pattern = int (input ("Enter the size of the pattern: "))
+i = 0
+
+while i < pattern:
+    for j in range(pattern):
+        print("*", end="")
+    print()
+    i+=1
