f52b8cd8ebbdb7731556064da580393d381d242b
mamado143 <midodahir123@gmail.com>
2025-12-14 19:05:52 +0300

    fix the files

---
+++ oop/book_class.py
@@ -0,0 +1,20 @@
+class Book:
+    """A class representing a book with magic methods."""
+    
+    def __init__(self, title, author, year):
+        """Initialize a Book instance with title, author, and year."""
+        self.title = title
+        self.author = author
+        self.year = year
+    
+    def __del__(self):
+        """Destructor that prints a message when the book is deleted."""
+        print(f"Deleting {self.title}")
+    
+    def __str__(self):
+        """Return a user-friendly string representation of the book."""
+        return f"{self.title} by {self.author}, published in {self.year}"
+    
+    def __repr__(self):
+        """Return an official string representation that can recreate the Book instance."""
+        return f"Book('{self.title}', '{self.author}', {self.year})"
